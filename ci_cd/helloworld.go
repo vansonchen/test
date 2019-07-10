f06ac5f7a13789fc7f8e99ab7d942767838806b7
vansonchen <vanson_chen@mail.chungyo.net>
2019-07-10 10:38:15 +0800

    test15

---
+++ ci_cd/helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"  
+)
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello!!\n")
+}
+
+func main(){
+    http.HandleFunc("/", myHandler)     //  設定訪問路由
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
